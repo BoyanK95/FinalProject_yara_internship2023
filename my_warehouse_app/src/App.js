4ef1e9c0d547752039fd3f3c2a52987157a0adb1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-04 15:55:15 +0300

    Set up initial architecture of the project

---
+++ my_warehouse_app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Helllo there</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ my_warehouse_app/src/pages/WarehousesPage/WarehouseMovementsPage.js
@@ -0,0 +1,9 @@
+function WarehouseMovementsPage() {
+    return (
+        <div>
+            <h1>Waregouses Page</h1>
+        </div>
+    );
+}
+
+export default WarehouseMovementsPage;
