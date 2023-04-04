9e4941c3e06032ea417a1a5c875fcc4cadd2c230
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-04 22:22:29 +0300

    Added toggle side bar functionality

--- my_warehouse_app/src/components/Layout/Layout.js
+++ my_warehouse_app/src/components/Layout/Layout.js
@@ -1,14 +1,32 @@
 import { Fragment } from 'react';
 import SideNavbar from './SideNavBar/SideNavBar';
 import TopHeader from './TopHeader/TopHeader';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
+import { useState } from 'react';
 
 const Layout = ({ children }) => {
+    const [sideBarIsVisible, setSideBarIsVisible] = useState(false);
+
+    function setSideBarIsVisibleHandler() {
+        setSideBarIsVisible(true);
+    }
+
+    function toggleSideBarIsNotVisibleHandler() {
+        setSideBarIsVisible(!sideBarIsVisible);
+    }
+
     return (
         <Fragment>
-            <TopHeader />
-            <SideNavbar />
+            <div
+                className={classes.mainWrapper}
+                onMouseEnter={setSideBarIsVisibleHandler}
+                onClick={toggleSideBarIsNotVisibleHandler}
+                // onMouseLeave={setSideBarIsNotVisibleHandler}
+            >
+                <TopHeader />
+            </div>
             <main className={classes.main}>{children}</main>
+            {sideBarIsVisible && <SideNavbar />}
         </Fragment>
     );
 };
