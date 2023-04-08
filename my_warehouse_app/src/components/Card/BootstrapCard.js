0e66f09d10094646a74099af991b67ddc06b1ce1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-08 20:39:26 +0300

    Refactored ProductPage and styled products container

--- my_warehouse_app/src/components/Card/BootstrapCard.js
+++ my_warehouse_app/src/components/Card/BootstrapCard.js
@@ -3,8 +3,8 @@ import Card from 'react-bootstrap/Card';
 
 function BootsrapCard({children, title}) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card style={{ width: '18rem', }}>
+      <Card.Img variant="top" src="https://www.4me.com/wp-content/uploads/2018/01/4me-icon-product.png" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>
--- my_warehouse_app/src/pages/ProductsPage/ProductsPage.js
+++ my_warehouse_app/src/pages/ProductsPage/ProductsPage.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+import BootsrapCard from '../../components/Card/BootstrapCard';
+import Button from 'react-bootstrap/Button';
 import { InfinitySpiner } from '../../components/LoadingSpiner/InfinitySpiner';
 import useHttp from '../../hooks/use-http';
-import classes from './ProductPage.module.css'
+import classes from './ProductPage.module.css';
 
 function ProductsPage() {
+    const [productsAreVisible, setProductsAreVisible] = useState(false);
     const { data, error, isLoading } = useHttp('http://localhost:3001/products', 'GET', null, 1000);
 
     console.log(data);
@@ -10,7 +14,15 @@ function ProductsPage() {
     return (
         <div className='centered'>
             <h1 className='title'>Products Page</h1>
-            <div className={classes.container}>
+            <div>
+                <p>
+                    "Welcome to the products page, where you can manage and update all your products in one place.
+                    Browse through your inventory, edit details, and add new items to your collection. Keep your
+                    customers engaged and satisfied with the latest products available at your store. Let's get
+                    started!"
+                </p>
+            </div>
+            <div>
                 {isLoading && <InfinitySpiner />}
                 {!isLoading && error && (
                     <>
@@ -18,7 +30,19 @@ function ProductsPage() {
                         <h4>{error?.response.statusText}!</h4>
                     </>
                 )}
+                {data && !isLoading && (
+                    <div className={classes.container}>
+                        {data.map((product) => {
+                            return (
+                                <div className={classes.product} key={product.id}>
+                                    <BootsrapCard title={product.name}>{product.description}</BootsrapCard>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
             </div>
+            <Button>Show Products</Button>
         </div>
     );
 }
