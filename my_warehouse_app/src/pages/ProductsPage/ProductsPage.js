cef8c17e00c8b06de30d23929dd5f8e77766a2fa
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-10 13:54:09 +0300

    Added add product Module functionality to ProductPage

--- my_warehouse_app/src/pages/ProductsPage/ProductsPage.js
+++ my_warehouse_app/src/pages/ProductsPage/ProductsPage.js
@@ -1,15 +1,21 @@
-import { useState } from 'react';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Button from 'react-bootstrap/Button';
-import useHttp from '../../hooks/use-http';
-import classes from './ProductPage.module.css';
 import Content from '../../components/Content/Content';
+import Modal from '../../components/Modal/Modal';
+import Card from 'react-bootstrap/Card';
+import AddItemForm from '../../components/Form/AddItemForm/AddItemForm';
 import { BlockSpiner } from '../../components/LoadingSpiner/BlocksSpiner';
+import useHttp from '../../hooks/use-http';
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import classes from './ProductPage.module.css';
 // import ItemComponent from '../../components/ItemComponent/ItemComponent';
 
 function ProductsPage() {
     const [productsAreVisible, setProductsAreVisible] = useState(false);
+    const [isAddingProduct, setIsAddingProduct] = useState(false);
     const { data, error, isLoading } = useHttp('http://localhost:3001/products', 'GET', null, 1000);
+    const history = useHistory();
 
     let btnText = 'Show Products';
     if (productsAreVisible) {
@@ -18,6 +24,16 @@ function ProductsPage() {
 
     function productsAreVisibleToggler() {
         setProductsAreVisible(!productsAreVisible);
+    }
+
+    function showAddProductToggler() {
+        setIsAddingProduct(!isAddingProduct);
+    }
+
+    function addWarehouseHandler(data) {
+        console.log(data);
+        history.push('/warehouses');
+        setIsAddingProduct(false);
     }
 
     console.log(data);
@@ -65,7 +81,35 @@ function ProductsPage() {
                     {data && data.length === 0 && !isLoading && !error && <h4>There are no products to display!</h4>}
                 </div>
             )}
-            <Button className={classes.button} onClick={productsAreVisibleToggler}>{btnText}</Button>
+            {isAddingProduct && (
+                <Modal onClose={showAddProductToggler}>
+                    <Card className={classes.addItemCard}>
+                        <Card.Body>
+                            <div>
+                                <h3 style={{ textAlign: 'center' }}>Add Warehouse</h3>
+                                <AddItemForm
+                                    onSubmit={addWarehouseHandler}
+                                    firstLabel='Product Name'
+                                    secondLabel='Unit Of Measure'
+                                    thirdLabel='Picture*'
+                                    fourthLabel='Description*'
+                                    numberLabel='Quantity'
+                                    hazardousLabel='Hazardous'
+                                    goTo='warehouses'
+                                />
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Modal>
+            )}
+            <div>
+                <Button className={classes.button} onClick={productsAreVisibleToggler}>
+                    {btnText}
+                </Button>
+                <Button variant='success' onClick={showAddProductToggler}>
+                    Add Product
+                </Button>
+            </div>
         </div>
     );
 }
